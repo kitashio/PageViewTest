11ce7710dde2cc29ab85cd5feb87134eb360dbc9
kitashio <kitashio.pr@gmail.com>
2022-03-23 21:34:07 +0900

    PageViewTest

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:roulette/roulette.dart';
 import 'package:valentine_choco_app/result_page.dart';
 
 void main() {
@@ -29,27 +27,16 @@ class RoulettePage extends StatefulWidget {
   State<RoulettePage> createState() => _RoulettePageState();
 }
 
-class _RoulettePageState extends State<RoulettePage> with SingleTickerProviderStateMixin {
-  late RouletteController _controller;
-  final bool _clockwise = true;
-
-  @override
-  void initState() {
-    _controller = RouletteController(
-        group: RouletteGroup([
-          const RouletteUnit.noText(color: Color.fromRGBO(70, 17, 17, 100)),
-          const RouletteUnit.noText(color: Color.fromRGBO(76, 0, 39, 100)),
-          const RouletteUnit.noText(color: Color.fromRGBO(227, 183, 160, 100)),
-          const RouletteUnit.noText(color: Color.fromRGBO(255, 237, 219, 100)),
-        ]),
-        vsync: this
-    );
-
-    super.initState();
-  }
+class _RoulettePageState extends State<RoulettePage>  {
 
   @override
   Widget build(BuildContext context) {
+
+    var _pages = [
+      Text("1！",),//0
+      Text("2！",),//1
+      Text("3！",),//2
+    ];
 
     return Scaffold(
       appBar: AppBar(
@@ -60,36 +47,38 @@ class _RoulettePageState extends State<RoulettePage> with SingleTickerProviderSt
           alignment: Alignment.center,
           child: Column(
             children: [
-              const SizedBox(height: 20),
-              Image.asset('images/valentine_title.png', height: 150,),
-              const SizedBox(height: 20),
-              SizedBox(
-                width: 260,
-                height: 260,
-                child: Roulette(
-                  controller: _controller,
-                  style: const RouletteStyle(
-                    centerStickerColor: Colors.brown,
-                    dividerThickness: 4,
-                  ),
-                ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: () async {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ResultPage(0),
+                      )
+                  );
+                } ,
+                child: _pages[0]
               ),
               const SizedBox(height: 50),
               ElevatedButton(
                 onPressed: () async {
-                  await _controller.rollTo(
-                    2,
-                    clockwise: _clockwise,
-                    offset: Random().nextDouble(),
-                  );
                   Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => const ResultPage(),
+                      MaterialPageRoute(builder: (context) => ResultPage(1),
                       )
                   );
                 } ,
-                child: const Text("受け取ってね！",
-                ),
+                child: _pages[1]
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: () async {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ResultPage(2),
+                      )
+                  );
+                } ,
+                child: _pages[2]
               ),
             ],
           ),
--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,40 +1,46 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 
 class ResultPage extends StatelessWidget{
-  const ResultPage({Key? key}) : super(key: key);
 
-  get math => null;
+  final int curretIndex;
+  const ResultPage(this.curretIndex,{Key? key,}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
 
-    String _nowword;
-    List<String> words = [
-      'images/girichocoyade.png',
-      'images/honnmeiyade.png',
-      'images/taiyakiyade.png',
-      'images/tomochocoyade.png',
+    var _pages = [
+      Container(
+        color: Colors.blue,
+        child: const Center(child: Text("1！",)),
+      ),
+      Container(
+        color: Colors.green,
+        child: const Center(child: Text("2！",)),
+      ),
+      Container(
+        color: Colors.pink,
+        child: const Center(child: Text("3！",)),
+      ),
     ];
-    var r = Random(DateTime.now().millisecondsSinceEpoch);
-    _nowword = words[r.nextInt(words.length)];
+
+    PageController _controller = PageController(initialPage: curretIndex);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Happy Valentine!'),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Image.asset(_nowword,
-              height: 300,
-              width: 300,),
-            const Text('だよ！',
-              style: TextStyle(
-                fontSize: 50,
-              ),
+      body: SafeArea(
+        child: Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: PageView(
+              controller: _controller,
+              children: [
+                for (var i = 0; i < 3; i++)
+                  _pages[i]
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
